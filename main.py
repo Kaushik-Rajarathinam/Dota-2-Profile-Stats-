d60c3d168f9c71486c30839d93e6e7ff929c90c1
Kaushik Rajarathinam <upolies345@gmail.com>
2023-12-22 19:56:00 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+#To Be Finished
+
+from validate import verify
+
+def prompt():
+    a = input('Input Steam Username or 64-bit ID: ')
+    verify(a)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    prompt()
